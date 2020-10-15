4946c46bbf83585f998c8b8aac07a325682ffbe8
Rory Liberty <roryliberty@gmail.com>
2020-10-15 16:51:11 -0500

    Individual game is working. Need to start on a 5-round game

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 const buttons = document.querySelectorAll(".button");
 let playerSelection = "";
+let computerSelection = "";
 let round = 0;
 
-function roundCount() {
-    for (i = 0; i <= 3; i++) {
-        round++;
-        document.getElementById("round-number").innerHTML = round;
-    }
-}
+document.getElementById("round-number").innerHTML = round;
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
         console.log(e.target.id);
-
+        
         if (e.target.id === "btnRock") {
-            console.log("Rock chosen by Player");
-            playerPlay("rock");                      
-            console.log(playerSelection);
+            playerPlay("rock");
+            document.getElementById("playerChoice").innerHTML = playerSelection;
+            computerPlay();
+            document.getElementById("computerChoice").innerHTML = computerSelection;                    
         } else if (e.target.id === "btnPaper") {
-            console.log("Paper chosen by Player");
             playerPlay("paper");
-            console.log(playerSelection);
+            document.getElementById("playerChoice").innerHTML = playerSelection;
+            computerPlay();
+            document.getElementById("computerChoice").innerHTML = computerSelection;
         } else if (e.target.id === "btnScissors") {
-            console.log("Scissors chosen by Player");
             playerPlay("scissors");
-            console.log(playerSelection);
+            document.getElementById("playerChoice").innerHTML = playerSelection;
+            computerPlay();
+            document.getElementById("computerChoice").innerHTML = computerSelection;
         }
-
-        roundCount();
-        console.log(round);
+        
+        playRound(playerSelection, computerSelection);
     });
 });
 
@@ -41,32 +39,43 @@ function computerPlay() {
     let rps = Math.floor(Math.random() * 3);
 
     if (rps == 0) {
-        return "rock"
+        computerSelection = "rock";
     } else if (rps == 1) {
-        return "paper"
+        computerSelection = "paper";
     } else {
-        return "scissors"
+        computerSelection = "scissors";
     }
-} 
-
-function playRound(playerSelection, computerSelection) {
+}
+// maybe I can use case: instead of if/else loop 
+function playRound(playerSelection, computerSelection) {    
     if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a tie!"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats Rock"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats Scissors"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats Rock"
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "It's a tie!"
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats paper"
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats Scissors"
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beats paper"
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "It's a tie!"
+        console.log("Tie: rock/rock");
+        document.getElementById("result").innerHTML = "Tie: rock/rock";
+    }else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("Win: rock/scissors");
+        document.getElementById("result").innerHTML = "Win: rock/scissors";
+    }else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Lose: rock/paper");
+        document.getElementById("result").innerHTML = "Lose: rock/paper"
+    }else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Lose: scissors/rock");
+        document.getElementById("result").innerHTML = "Lose: scissors/rock"
+    }else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("Tie: scissors/scissors");
+        document.getElementById("result").innerHTML = "Tie: scissors/scissors"
+    }else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Win: scissors/paper");
+        document.getElementById("result").innerHTML = "Win: scissors/paper"
+    }else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Win: paper/rock");
+        document.getElementById("result").innerHTML = "Win: paper/rock"
+    }else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("Lose: paper/scissors");
+        document.getElementById("result").innerHTML = "Lose: paper/scissors"
+    }else if (playerSelection === "paper" && computerSelection === "paper") {
+        console.log("Tie: paper/paper");
+        document.getElementById("result").innerHTML = "Tie: paper/paper"
+    }else {
+        return;
     }
 }
