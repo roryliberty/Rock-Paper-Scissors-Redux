c8d8fe115b422292fad01212e9a5ede05abe0950
Rory Liberty <roryliberty@gmail.com>
2020-10-21 11:03:58 -0500

    More cleaning with Chris' help

--- script.js
+++ script.js
@@ -29,30 +29,26 @@ buttons.forEach((button) => {
 
 function getChoices(press) {
     let theButton = press;
+
     if (theButton === "btnRock") {
-        playerSelection = "rock";
-        document.getElementById("playerChoice").innerHTML = playerSelection;
-        computerPlay();
-        document.getElementById("computerChoice").innerHTML = computerSelection;                    
+        playerSelection = "rock";                   
     } else if (theButton === "btnPaper") {
         playerSelection = "paper";
-        document.getElementById("playerChoice").innerHTML = playerSelection;
-        computerPlay();
-        document.getElementById("computerChoice").innerHTML = computerSelection;
     } else if (theButton === "btnScissors") {
         playerSelection = "scissors";
-        document.getElementById("playerChoice").innerHTML = playerSelection;
-        computerPlay();
-        document.getElementById("computerChoice").innerHTML = computerSelection;
     }
+
+    document.getElementById("playerChoice").innerHTML = playerSelection;
+    computerPlay();
+    document.getElementById("computerChoice").innerHTML = computerSelection;
 }
 
 function computerPlay() {
     let rps = Math.floor(Math.random() * 3);
 
-    if (rps == 0) {
+    if (rps === 0) {
         computerSelection = "rock";
-    } else if (rps == 1) {
+    } else if (rps === 1) {
         computerSelection = "paper";
     } else {
         computerSelection = "scissors";
@@ -62,32 +58,31 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {    
     if (playerSelection === "rock" && computerSelection === "rock") {
         document.getElementById("result").innerHTML = "Tie: rock/rock";
-    }else if (playerSelection === "rock" && computerSelection === "scissors") {
-        document.getElementById("result").innerHTML = "Win: rock/scissors";
-        playerScore += 1;
-    }else if (playerSelection === "rock" && computerSelection === "paper") {
-        document.getElementById("result").innerHTML = "Lose: rock/paper";
-        computerScore +=1;
-    }else if (playerSelection === "scissors" && computerSelection === "rock") {
-        document.getElementById("result").innerHTML = "Lose: scissors/rock";
-        computerScore += 1;
-    }else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        document.getElementById("result").innerHTML = "Tie: scissors/scissors"
-    }else if (playerSelection === "scissors" && computerSelection === "paper") {
-        document.getElementById("result").innerHTML = "Win: scissors/paper";
-        playerScore += 1;
-    }else if (playerSelection === "paper" && computerSelection === "rock") {
-        document.getElementById("result").innerHTML = "Win: paper/rock";
-        playerScore += 1;
-    }else if (playerSelection === "paper" && computerSelection === "scissors") {
-        document.getElementById("result").innerHTML = "Lose: paper/scissors";
-        computerScore += 1;
-    }else if (playerSelection === "paper" && computerSelection === "paper") {
-        document.getElementById("result").innerHTML = "Tie: paper/paper";
-    }else {
-        return;
-    }
+    }       else if (playerSelection === "rock" && computerSelection === "scissors") {
+                document.getElementById("result").innerHTML = "Win: rock/scissors";
+                playerScore += 1;
+            }   else if (playerSelection === "rock" && computerSelection === "paper") {
+                    document.getElementById("result").innerHTML = "Lose: rock/paper";
+                    computerScore +=1;
+                }   else if (playerSelection === "scissors" && computerSelection === "rock") {
+                        document.getElementById("result").innerHTML = "Lose: scissors/rock";
+                        computerScore += 1;
+                    }   else if (playerSelection === "scissors" && computerSelection === "scissors") {
+                            document.getElementById("result").innerHTML = "Tie: scissors/scissors"
+                        }   else if (playerSelection === "scissors" && computerSelection === "paper") {
+                                document.getElementById("result").innerHTML = "Win: scissors/paper";
+                                playerScore += 1;
+                            }   else if (playerSelection === "paper" && computerSelection === "rock") {
+                                    document.getElementById("result").innerHTML = "Win: paper/rock";
+                                    playerScore += 1;
+                                }   else if (playerSelection === "paper" && computerSelection === "scissors") {
+                                        document.getElementById("result").innerHTML = "Lose: paper/scissors";
+                                        computerScore += 1;
+                                    }   else if (playerSelection === "paper" && computerSelection === "paper") {
+                                            document.getElementById("result").innerHTML = "Tie: paper/paper";
+                                        }   else {
+                                                return;
+                                            }
 
-    document.getElementById("totals").innerHTML = ("Total goes here: " + playerScore +
-            " - " + computerScore);
+    document.getElementById("totals").innerHTML = ("Total goes here: " + playerScore + " - " + computerScore);
 }
